f2b32867f0a01ceeb038d70d8b86cf822db4dc30
Joshua Neves <joshie.cad@gmail.com>
2024-09-03 11:53:36 +0100

    Added Media queries for Company info section, and Product range section
    - Product range, Company info, and footer sections were all using inline grids for horizontal layouts. These were changed to be Flex displays with flex directions of row while on larger screens and flex directions of column on smaller screens.
    
    - each section was adjusted to be responsive to screen sizes.
    
    - Only the Footer section still needs to be made more responsive.

--- src/CompanyInfo.js
+++ src/CompanyInfo.js
@@ -4,7 +4,7 @@ import baker from './assets/baker.jpg';
 export const CompanyInfo = () =>{
     return(
         <div className='compInfo'>
-            <p>What started as a small Macaron shop in a small city has become a large scale delivery system for the whole nation!</p>
+            <p>' What started as a small Macaron shop in a small city has become a large scale delivery system for the whole nation! '</p>
             <div className='compImg'>
                 <img src={baker} alt='baker'/>
             </div>
