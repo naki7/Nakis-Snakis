365626666dfc90ff17ba57ee361aba50ac56937d
Joshua Neves <joshie.cad@gmail.com>
2024-09-04 17:09:48 +0100

    Added useState to Footer section
    - Implemented useState to add a .isHIdden className so that on small devices the extra information (p tags) are hidden to save space in the footer section
    
    - also changed some width values in the company info, hero and navbar sections from 100vw to 100% as this was causing these sections to bleed over into space outside of the screen

--- src/Footer.js
+++ src/Footer.js
@@ -1,24 +1,46 @@
 import './Footer.css';
+import { useState } from 'react';
 
 export const Footer = () =>{
+
+    const [factory, setFactory] = useState(false);
+    const [office, setOffice] = useState(false);
+    const [contact, setContact] = useState(false);
+
+    const switchOpen = (option) =>{
+         if(option === 'fact'){
+            setFactory(!factory);
+            setOffice(false);
+            setContact(false);
+          } else if(option === 'offi'){
+            setOffice(!office);
+            setFactory(false);
+            setContact(false);
+        } else if('cont'){
+            setContact(!contact);
+            setFactory(false);
+            setOffice(false);
+        }
+    }
+
     return(
         <div className="Footer">
             <div className='information'>
                 <section>
                     <div className="factory">
-                        <h3>Factory Address</h3>
-                        <p className="address">25 Naki Street,<br/>Snack City,<br/>West Snacklandia,<br/>United Snackdom</p>         
+                        <h3 onClick={(e) => switchOpen('fact')}>Factory Address</h3>
+                        <p className={factory ? "address" : "address isHidden"}>25 Naki Street,<br/>Snack City,<br/>West Snacklandia,<br/>United Snackdom</p>         
                     </div>
                     <div className='office'>
-                        <h3>Office Address</h3>
-                        <p className="address">16 Naki Avenue,<br/>Snack City,<br/>West Snacklandia,<br/>United Snackdom</p>   
+                        <h3 onClick={(e) => switchOpen('offi')}>Office Address</h3>
+                        <p className={office ? "address" : "address isHidden"}>16 Naki Avenue,<br/>Snack City,<br/>West Snacklandia,<br/>United Snackdom</p>   
                     </div>
                 </section>
                 <section>
                     <div className="contact">
-                        <h3>You can reach us on:</h3>
-                        <p>nakissnakis@snackmail.co.snacks</p>
-                        <p>(+099) 196 1960</p>
+                        <h3 onClick={(e) => switchOpen('cont')}>You can reach us on:</h3>
+                        <p className={contact ? "" : "isHidden"}>nakissnakis@snackmail.co.snacks</p>
+                        <p className={contact ? "" : "isHidden"}>(+099) 196 1960</p>
                     </div>
                     <div className="portfolio">
                         <h3>This is a portfolio piece:</h3>
