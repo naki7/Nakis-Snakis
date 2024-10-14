57e83848c18ed12e6453aff8ad1320125dfdaf02
Joshua Neves <joshie.cad@gmail.com>
2024-10-14 11:38:01 +0100

    Added shopping cart
    -included icon of shopping cart
      - added on total of items currently in shopping cart

--- src/Cart.js
+++ src/Cart.js
@@ -1,7 +1,11 @@
+import React from "react";
+import './Cart.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 export const Cart = (Props) =>{
     const { straw, grape, caram, menth } = Props;
+    let total = straw + grape + caram + menth;
 
     return(
         <div>{straw} {grape} {caram} {menth}
@@ -9,7 +13,10 @@ export const Cart = (Props) =>{
 
             </div>
             <div className="icon">
-                <FontAwesomeIcon icon="fa-solid fa-cart-arrow-down" />
+                <FontAwesomeIcon icon={faCartShopping} className="sCart" />
+                <div className="count">
+                    <p>{total}</p>
+                </div>
             </div>
         </div>
     )
